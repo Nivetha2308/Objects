5f9b545fbaff604eeb6a3707a8debe9c6ead934e
Nivetha2308 <nivetha23mega@gmail.com>
2024-07-06 06:34:51 +0530

    first commit

---
+++ Ch3_pgm_01.js
@@ -0,0 +1,42 @@
+// Using variables to represent a book
+
+var bookTitle;
+var bookAuthor;
+
+bookTitle = "The Hobbit";
+bookAuthor = "J. R. R. Tolkien";
+
+console.log(bookTitle + " by " + bookAuthor);
+
+var bookTitle2;
+var bookAuthor2;
+
+bookTitle2 = "1984";
+bookAuthor2 = "George Orwell";
+
+console.log(bookTitle2 + " by " + bookAuthor2);
+
+var bookTitle3;
+var bookAuthor3;
+
+bookTitle3 = "To Kill a Mockingbird";
+bookAuthor3 = "Harper Lee";
+
+console.log(bookTitle3 + " by " + bookAuthor3);
+
+
+
+
+/* Further Adventures
+ *
+ * 1) Declare variables for a second book
+ *    and assign them values.
+ *
+ * 2) Add code to log its details to the console.
+ *
+ * 3) Repeat steps 1 and 2 for a third book.
+ *
+ * 4) Consider the code needed for 10 books.
+ *    For 100 books.
+ *
+ */
---
+++ Ch3_pgm_12.js
@@ -0,0 +1,39 @@
+// A blog post
+
+var post = {
+	id : 1,
+	title : "My Crazy Space Adventure",
+	author : "Philae",
+	created : "2015-06-21",
+	body : "You will not believe where I just woke up!! Only on a comet..."
+};
+
+console.log("Post Title: " + post.title);
+console.log("Author: " + post.author);
+console.log("Created Date: " + post.created);
+
+
+var post2 = {
+    id: 2,
+    title: "Exploring the Martian Landscape",
+    author: "Curiosity Rover",
+    created: "2012-08-06",
+    body: "Today I discovered some intriguing rock formations..."
+};
+
+console.log("--- Second Post Details ---");
+console.log("Post Title: " + post2.title);
+console.log("Author: " + post2.author);
+console.log("Created Date: " + post2.created);
+console.log("Body: " + post2.body);
+
+
+
+
+/* Further Adventures
+ *
+ * 1) Display some of the properties.
+ *
+ * 2) Add a second post.
+ *
+ */
